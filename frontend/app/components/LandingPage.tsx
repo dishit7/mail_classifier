1a43d2727d123ef7b5096976748770f86292c217
dishit7 <hanmadishit74@gmail.com>
2024-06-14 18:39:20 +0530

    Add types for MailIcon props

--- frontend/app/components/LandingPage.tsx
+++ frontend/app/components/LandingPage.tsx
@@ -4,6 +4,7 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import Link from "next/link"
+import { JSX, SVGProps } from "react"
 export default function Component() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
@@ -69,7 +70,7 @@ export default function Component() {
   )
 }
 
-function MailIcon(props) {
+function MailIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
